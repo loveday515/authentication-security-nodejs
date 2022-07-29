33c0149e90007a3f716fb3ac0656ac93485b7388
loveday515 <lovedayikegbulam515@gmail.com>
2022-07-29 18:25:09 +0100

    Allow user to submit secret

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 	googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -105,12 +106,41 @@ app.get("/register", (req, res) => {
 	res.render("register");
 });
 
-app.get("/secrets", (req, res) => {
-    if(req.isAuthenticated){
-        res.render("secrets");
-    }else{
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated) {
+        res.render("submit");
+    } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", (req, res) => {
+    const submittedSecret = req.body.secret
+
+    User.findById(req.user.id, (err, foundUser) => {
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret
+                foundUser.save(() => {
+                    res.redirect("secrets")
+                })
+            }
+        }
+    });
+})
+
+app.get("/secrets", (req, res) => {
+    User.find({"secret": {$ne: null}}, (err, foundUsers) => {
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecret: foundUsers});
+            }
+        }
+    });
 });
 
 app.get("/logout", (req, res) => {
